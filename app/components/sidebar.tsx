b5435fddab57a2944ae271abc5c89ee05b1e18b5
PIKAQIU101 <131634064+PIKAQIU101@users.noreply.github.com>
2023-04-25 12:47:08 +0800

    Update sidebar.tsx

--- app/components/sidebar.tsx
+++ app/components/sidebar.tsx
@@ -92,7 +92,7 @@ export function SideBar(props: { className?: string }) {
       <div className={styles["sidebar-header"]}>
         <div className={styles["sidebar-title"]}>ChatGPT Next</div>
         <div className={styles["sidebar-sub-title"]}>
-          Build your own AI assistant.
+          添加微信：Kylinisland，获得AI实战研习社知识星球25元优惠券。
         </div>
         <div className={styles["sidebar-logo"]}>
           <ChatGptIcon />
